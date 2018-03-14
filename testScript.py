19ccb55ea447bd550d23552b2b9162d8d7e8e7d3
gwiedeman <gregory.wiedeman1@gmail.com>
2018-03-14 16:25:57 -0400

    sort of works, don't really know how

--- testScript.py
+++ testScript.py
@@ -4,12 +4,10 @@ AS = ASpace()
 
 print (AS.resource(150).title)
 
-
-for note in AS.resource(150).notes:
+for note in AS.resource("150").notes:
 	if note.type == "abstract":
 		for text in note.content:
 			print (text)
-"""
-for ao in AS.resource(150).archival_objects:
+			
+for ao in AS.resource(151).archival_objects:
 	print (ao.title)
-"""
