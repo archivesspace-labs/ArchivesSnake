86cf0c83129f15468048ccc454500d85250b3ebd
William David Mayo ("Dave") <pobocks@gmail.com>
2018-04-07 20:16:34 -0400

    Radical simplification of JSONModel stuff.
    A relation is now pretty much a route and the params used, which can
    be iterated over for all records, or can be used to search for an
    individual record.
    
    JSONModel object now essentially try to do an API call if
    they don't have a matching key in their __json - this seems to pretty
    much handle repository-scoping without special casing or obvious
    breakage.  It's actually surprising how much of the "read"
    functionality of the API this covers. I don't doubt we'll need some
    special casing or want to collapse some routes, and we'll definitely
    need to handle trees specially, for example, but the abstraction is
    able to cover most objects index and show methods.

--- asnake/aspace/__init__.py
+++ asnake/aspace/__init__.py
@@ -8,9 +8,6 @@ class ASpace():
 
         # this happens when you call ASpace()
         def __init__(self, **config):
-                # Repository will default to 2 if not provided
-                self.repository = config.pop('repository', '2')
-
                 # Connect to ASpace using .archivessnake.yml
                 self.__client = ASnakeClient(**config)
                 self.__client.authorize()
@@ -23,19 +20,12 @@ class ASpace():
         # this automatically sets attributes to ASpace(), so you can ASpace().resources, etc.
         def __getattr__(self, attr):
                 if not attr.startswith('_'):
-                        # This sets plural attributes, like resources and archival_objects
-                        # Not sure if this is safe
-                        if attr.lower().endswith("s"):
-                                shortCalls = ["repositories", "locations", "subjects", "users", "vocabularies", "location_profiles", "container_profiles"]
-                                #for calls without repositories in them
-                                if attr in shortCalls:
-                                        return JSONModelRelation(self.__client.get("/" + str(attr), params= {"all_ids": True}).json(), self.__client, self.repository, attr)
-                                else:
-                                        return JSONModelRelation(self.__client.get("/repositories/" + str(self.repository) + "/" + str(attr), params={"all_ids": True}).json(), self.__client, self.repository, attr)
+                        return JSONModelRelation("/{}".format(attr), params={"all_ids": True}, client = self.__client)
 
         @property
         def resources(self):
                 '''return all resources from every repo'''
+
                 repo_uris = [r['uri'] for r in self.__client.get('repositories').json()]
                 for resource in chain(*[self.__client.get_paged('{}/resources'.format(uri)) for uri in repo_uris]):
                         yield JSONModelObject(resource, self.__client)
@@ -60,9 +50,3 @@ class ASpace():
                                 return JSONModelObject(self.__client.get(refList["archival_objects"][0]["ref"]).json(), self.__client)
                 #its a uri number
                 return JSONModelObject(self.__client.get("repositories/" + self.repository + "/archival_objects/" + str(id)).json(), self.__client)
-
-        def agents(self, type, id = None):
-                if id == None:
-                        return JSONModelRelation(self.__client.get("/agents/" + str(type) + "?all_ids=true").json(), self.__client, self.repository, type)
-                else:
-                        return JSONModelObject(self.__client.get("/agents/" + str(type) + "/" + str(id)).json(), self.__client)
--- asnake/jsonmodel/__init__.py
+++ asnake/jsonmodel/__init__.py
@@ -1,14 +1,19 @@
-# Classes dealing with JSONModel imports
-class JSONModelObject:
-        '''A wrapper over the JSONModel representation of a single object in ArchivesSpace.'''
-        __default_client = None
+from itertools import chain
 
-        @classmethod
+# Base metaclass for shared functionality
+class JSONModel(type):
+        def __init__(cls, name, parents, dct):
+                cls.__default_client = None
+
         def default_client(cls):
             if not cls.__default_client:
                     from asnake.client import ASnakeClient
                     cls.__default_client = ASnakeClient()
-            return __default_client
+            return cls.__default_client
+
+# Classes dealing with JSONModel imports
+class JSONModelObject(metaclass=JSONModel):
+        '''A wrapper over the JSONModel representation of a single object in ArchivesSpace.'''
 
         def __init__(self, json_rep, client = None):
                 self.__json = json_rep
@@ -40,11 +45,14 @@ class JSONModelObject:
                 self.reify()
                 if not key.startswith('_'):
                         if not key in self.__json.keys():
-                                repository = self.__json["repository"]["ref"].split("/repositories/")[1]
-                                return JSONModelRelation(self.__json, self.__client)
+                                uri = "/".join((self.__json['uri'].rstrip("/"), key,))
+                                if self.__client.head(uri, params={"all_ids":True}).status_code == 200:
+                                        return JSONModelRelation(uri, client=self.__client)
+                                else:
+                                        raise AttributeError("'{}' has no attribute '{}'".format(repr(self), key))
 
                 if isinstance(self.__json[key], list):
-                        return JSONModelRelation(self.__json[key], self.__client)
+                        return [JSONModelObject(obj, self.__client) for obj in self.__json[key]]
                 elif isinstance(self.__json[key], dict):
                         return JSONModelObject(self.__json[key], self.__client)
                 else:
@@ -62,50 +70,18 @@ class JSONModelObject:
                 f.write(json.dumps(self.__json, indent=2))
                 f.close
 
+class JSONModelRelation(metaclass=JSONModel):
+        def __init__(self, uri, params = {}, client = None):
+                self.uri = uri
+                self.client = client or type(self).default_client()
+                self.params = params
 
-#Returns a generator object to stream lists of objects
-def JSONModelRelation(json_list, client, repository=None, call=None):
+        def __iter__(self):
+                for jm in self.client.get_paged(self.uri, params=self.params):
+                        yield JSONModelObject(jm, self.client)
 
-        if isinstance(json_list, list):
-                #this is for a list of ids to call
-                for item in json_list:
-                        if isinstance(item, str):
-                                yield item
-                        if isinstance(item, int):
-                                #check for agents, because its a different call
-                                shortCalls = ["locations", "subjects", "users", "vocabularies", "location_profiles", "container_profiles"]
-                                agentTypes = ["corporate_entities", "families", "people", "software"]
-                                if call in shortCalls:
-                                        object = client.get(call + "/" + str(item)).json()
-                                elif call in agentTypes:
-                                        object = client.get("agents/" + call + "/" + str(item)).json()
-                                else:
-                                        object = client.get("repositories/" + repository + "/" + call + "/" + str(item)).json()
-                                yield JSONModelObject(object, client)
-                        else:
-                                yield JSONModelObject(item, client)
+        def __call__(self, myid):
+                return JSONModelObject(self.client.get("/".join((self.uri.rstrip("/"), str(myid),))).json(), self.client)
 
-        else:
-                        # for trees, like a list of children
-                        # check if resource or archival_object
-                        if json_list["jsonmodel_type"] == "resource":
-                                tree = client.get(json_list["uri"] + "/tree").json()["children"]
-                                for child in tree:
-                                        childObject = client.get(child["record_uri"]).json()
-                                        yield JSONModelObject(childObject, client)
-                        else:
-                                tree = client.get(json_list["resource"]["ref"] + "/tree").json()["children"]
-                                for child in findChild(tree, json_list["uri"], None):
-                                        yield JSONModelObject(lient.get(child["record_uri"]).json(), client)
-
-# this finds children within trees
-# I think there's better ways of doing this in 2.0+
-def findChild(tree, uri, childrenObject):
-        for child in tree["children"]:
-                if child["record_uri"] == uri:
-                        childrenObject = makeObject(child)
-                elif len(child["children"]) < 1:
-                        pass
-                else:
-                        childrenObject = findChild(child, uri, childrenObject)
-        return childrenObject
+        def __getattr__(self, key):
+                return type(self)("/".join((self.uri, key,)), params=self.params, client=self.client)
