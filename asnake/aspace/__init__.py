f0b46b11fc663d27c9b3b496d83a6dc423397ab3
William David Mayo ("Dave") <pobocks@gmail.com>
2018-04-12 20:26:37 -0400

    Rough attempt at 'by-external-id', seems ok.

--- asnake/aspace/__init__.py
+++ asnake/aspace/__init__.py
@@ -2,16 +2,18 @@ from asnake.client import ASnakeClient
 from asnake.jsonmodel import JSONModelObject, JSONModelRelation
 from collections.abc import Sequence
 from itertools import chain
+from boltons.setutils import IndexedSet
 import json
 import re
 
+class ASnakeBadReturnCode: pass
 class ASpace():
     # this happens when you call ASpace()
     def __init__(self, **config):
         # Connect to ASpace using .archivessnake.yml
-        self.__client = ASnakeClient(**config)
-        self.__client.authorize()
-        m = re.match(r'\(v?(.+\))', self.__client.get('version').text)
+        self.client = ASnakeClient(**config)
+        self.client.authorize()
+        m = re.match(r'\(v?(.+\))', self.client.get('version').text)
         if m:
             self.version = m[1]
         else:
@@ -20,20 +22,41 @@ class ASpace():
     def __getattr__(self, attr):
         '''returns the JSONModelRelation representing the route with the same name as the attribute requested.'''
         if not attr.startswith('_'):
-            return JSONModelRelation("/{}".format(attr), params={"all_ids": True}, client = self.__client)
+            return JSONModelRelation("/{}".format(attr), params={"all_ids": True}, client = self.client)
 
     @property
     def resources(self):
         '''return all resources from every repo.'''
 
-        repo_uris = [r['uri'] for r in self.__client.get('repositories').json()]
-        for resource in chain(*[self.__client.get_paged('{}/resources'.format(uri)) for uri in repo_uris]):
-            yield JSONModelObject(resource, self.__client)
+        repo_uris = [r['uri'] for r in self.client.get('repositories').json()]
+        for resource in chain(*[self.client.get_paged('{}/resources'.format(uri)) for uri in repo_uris]):
+            yield JSONModelObject(resource, self.client)
 
     @property
     def agents(self):
         '''returns an AgentRelation.'''
-        return AgentRelation("/agents", {}, self.__client)
+        return AgentRelation("/agents", {}, self.client)
+
+
+    def by_external_id(self, external_id, record_types=None):
+        '''return any resources fetched from the 'by-external-id' route.
+
+Note: while the route will return differently depending on how many records are returned,
+this method deliberately flattens that out - it will _always_ return a generator, even if only
+one record is found.'''
+        params = {"eid": external_id}
+        if record_types: params['type[]'] = record_types
+
+        res = self.client.get('by-external-id', params=params)
+        if res.status_code == 404:
+            return []
+        elif res.status_code == 300: # multiple returns, bare list of uris
+            yield from (JSONModelObject({"ref": uri}, self.client) for uri in IndexedSet(res.json()))
+        elif res.status_code == 200: # single obj, redirects to obj with 303->200
+            yield JSONModelObject(res.json(), self.client)
+        else:
+            raise ASnakeBadReturnCode("by-external-id call returned '{}'".format(res.status_code))
+
 
 class ASNakeBadAgentType(Exception): pass
 
