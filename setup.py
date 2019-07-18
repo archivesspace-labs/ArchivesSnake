4e9be7b24855f6d8dc27bb9e79d4343700e32b9a
William David Mayo ("Dave") <pobocks@gmail.com>
2019-07-18 17:51:50 -0400

    Updated dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     long_description="""A client library for the ArchivesSpace REST API.""",
     author="ArchivesSnake Developer Group",
     author_email="asnake.developers@gmail.com",
-    version="0.8.1",
+    version="0.8.2",
     packages=find_packages(),
     zip_safe=False,
     classifiers=[
