86dda2d810d41251d00e4a4df39285bdbefb6b8a
Patrick <pgalligan@rockarch.org>
2020-06-23 14:17:23 -0400

    remove resp from init
    removing resp code from and placing in decorator

--- asnake/utils/decorators.py
+++ asnake/utils/decorators.py
@@ -5,6 +5,9 @@ def check_type(obj_type):
     def real_decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
+            if not isinstance(args[0], obj_type):
+                raise TypeError("{} is not a {}".format(args[0], obj_type))
+
             ao_uri = None
             # if object has .json(), replace with value of .json()
             if callable(getattr(ao_thingit, 'json', None)):
@@ -12,6 +15,7 @@ def check_type(obj_type):
 
             if isinstance(ao_thingit, str):
                 ao_uri = ao_thingit
+                
             elif isinstance(ao_thingit, Mapping):
                 ao_uri = ao_thingit.get("uri", ao_thingit.get("ref", None))
 
@@ -21,9 +25,6 @@ def check_type(obj_type):
             resp = client.get(ao_uri, params={'resolve': ['top_container::container_locations']})
             if resp.status_code != 200:
                 raise Exception("Unable to fetch archival object with resolved container locations")
-
-            if not isinstance(args[0], obj_type):
-                raise TypeError("{} is not a {}".format(args[0], obj_type))
             return func(*args, **kwargs)
         return wrapper
     return real_decorator
