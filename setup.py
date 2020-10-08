29507b93680c1045431f40e97d46ec7ca93890bb
William David Mayo ("Dave") <pobocks@gmail.com>
2020-10-08 17:02:57 -0400

    Release 0.9.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     long_description="""A client library for the ArchivesSpace REST API.""",
     author="ArchivesSnake Developer Group",
     author_email="asnake.developers@gmail.com",
-    version="0.8.5",
+    version="0.9.0",
     packages=find_packages(),
     zip_safe=False,
     classifiers=[
